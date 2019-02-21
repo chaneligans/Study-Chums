622cb4b4dffddc0dc772576138984abe25a9dfa3
jasmineslike <lili.xu@student.csulb.edu>
2019-02-20 16:04:21 -0800

    profileTest

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,11 @@
+import {MDCRipple} from '@material/ripple';
 const functions = require('firebase-functions');
+
+
+const selector = '.mdc-button, .mdc-icon-button, .mdc-card__primary-action';
+const ripples = [].map.call(document.querySelectorAll(selector), function(el) {
+  return new MDCRipple(el);
+});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
