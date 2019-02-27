07126c6a2ed5dbfa15c8463be46ede9e22a72c03
Austin Tao <32249785+MrAlphalfa97@users.noreply.github.com>
2019-02-27 00:08:24 -0800

    its a start, but i can get stuff from firestore

--- functions/app.js
+++ functions/app.js
@@ -8,18 +8,19 @@
         apiKey: "AIzaSyDFPHvR2SV7dodysPpvLUfIGp6huuBDf0A",
         authDomain: "study-chums.firebaseapp.com",
         databaseURL: "https://study-chums.firebaseio.com/",
+        projectId: 'study-chums',
         storageBucket: "study-chums.appspot.com"
     };
     firebase.initializeApp(config);
-    
+
 //    var database = firebase.database();
-//    
+//
 //    var userId = firebase.auth().currentUser.uid;
 //    return firebase.database().ref('/S/' + userId).once('value').then(function(snapshot) {
 //  var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
 //  // ...
 //});
-    
+
 
     //get elements
     const preObject = document.getElementById('Users');
@@ -27,10 +28,19 @@
     //Create reference
 
     const dbRefObject = firebase.database().ref().child('Users/1');
-    
+
     //Sync object changes
     dbRefObject.on('value',snap =>{
         preObject.innerText = JSON.stringify(snap.val(),null, 3);
     });
-    
+
+    //firestore
+    const fsRefObject = firebase.firestore().collection('users');
+    //prints to console each document in a collection (users)
+    fsRefObject.get().then(snapshot => {
+      snapshot.forEach(doc => {
+        console.log(doc.id, "=>", doc.data());
+      });
+    });
+
 }());
