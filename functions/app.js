64a69b8cf072b61c3818be086a035b179db342be
Samantha Wiley <samantha.wiley@student.csulb.edu>
2019-02-27 13:09:27 -0800

    Commented Out Errors--app.js

--- functions/app.js
+++ functions/app.js
@@ -1,53 +1,53 @@
-//test the cloud
+// //test the cloud
 
 
-(function(){
-    //var firebase = require("firebase");
-    //Initialize Firebase
-    const config = {
-        apiKey: "AIzaSyDFPHvR2SV7dodysPpvLUfIGp6huuBDf0A",
-        authDomain: "study-chums.firebaseapp.com",
-        databaseURL: "https://study-chums.firebaseio.com/",
-        projectId: 'study-chums',
-        storageBucket: "study-chums.appspot.com"
-    };
-    firebase.initializeApp(config);
+// (function(){
+//     //var firebase = require("firebase");
+//     //Initialize Firebase
+//     const config = {
+//         apiKey: "AIzaSyDFPHvR2SV7dodysPpvLUfIGp6huuBDf0A",
+//         authDomain: "study-chums.firebaseapp.com",
+//         databaseURL: "https://study-chums.firebaseio.com/",
+//         projectId: 'study-chums',
+//         storageBucket: "study-chums.appspot.com"
+//     };
+//     firebase.initializeApp(config);
 
-//    var database = firebase.database();
-//
-//    var userId = firebase.auth().currentUser.uid;
-//    return firebase.database().ref('/S/' + userId).once('value').then(function(snapshot) {
-//  var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
-//  // ...
-//});
-
-
-    //get elements
-    const preObject = document.getElementById('Users');
-
-    //Create reference
-
-    const dbRefObject = firebase.database().ref().child('Users');
-
-    //Sync object changes // for realtime
-    dbRefObject.on('value',snap =>{
-      snap.forEach(item => {
-        preObject.innerText += JSON.stringify(item.val(),null, 3);
-        var ids = item.key;
-        console.log("Object id:", ids);
-      })
-
-    });
-
-    //firestore
-    const fsRefObject = firebase.firestore().collection('users');
-    //prints to console each document in a collection (users)
-    fsRefObject.get().then(snapshot => {
-      snapshot.forEach(doc => {
-        console.log(doc.id, "=>", doc.data());
-      });
-    });
+// //    var database = firebase.database();
+// //
+// //    var userId = firebase.auth().currentUser.uid;
+// //    return firebase.database().ref('/S/' + userId).once('value').then(function(snapshot) {
+// //  var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+// //  // ...
+// //});
 
 
+//     //get elements
+//     const preObject = document.getElementById('Users');
 
-}());
+//     //Create reference
+
+//     const dbRefObject = firebase.database().ref().child('Users');
+
+//     //Sync object changes // for realtime
+//     dbRefObject.on('value',snap =>{
+//       snap.forEach(item => {
+//         preObject.innerText += JSON.stringify(item.val(),null, 3);
+//         var ids = item.key;
+//         console.log("Object id:", ids);
+//       })
+
+//     });
+
+//     //firestore
+//     const fsRefObject = firebase.firestore().collection('users');
+//     //prints to console each document in a collection (users)
+//     fsRefObject.get().then(snapshot => {
+//       snapshot.forEach(doc => {
+//         console.log(doc.id, "=>", doc.data());
+//       });
+//     });
+
+
+
+// }());
