df9a1aa7cb1c58c2948a8b954abb1a0fd7b6d148
chaneligans <37652117+chaneligans@users.noreply.github.com>
2019-03-15 23:19:25 -0700

    fixed signout

--- public/scripts/facebook_logout.js
+++ public/scripts/facebook_logout.js
@@ -1,9 +1,7 @@
 function facebookSignOut() {
     firebase.auth().signOut().then(function() {
-      FB.logout(function(response) {
-      // user is now logged out
-      });
         console.log('Signout successful');
+      location.href="index.html"
     }, function(error) {
         console.log('Signout failed')
     });
