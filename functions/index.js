fd3ae81544f8a1dc691757d8771145218ef2c6af
chaneligans <37652117+chaneligans@users.noreply.github.com>
2019-04-21 22:52:57 -0700

    more email debugging/styling

--- functions/index.js
+++ functions/index.js
@@ -68,7 +68,7 @@ exports.sendRequestEmail = functions.database.ref('Applications/{uid}/Sent/{valu
 
       mailOptions.subject = `You have a new match request! | ${APP_NAME}`;
 
-      mailOptions.html = "<html><head><style>@font-face { font-family: 'Nunito'; font-style: normal; font-weight: 400; src: local('Nunito Regular'), local('Nunito-Regular'), url(https://fonts.gstatic.com/s/nunito/v10/XRXV3I6Li01BKofIOuaBXso.woff2) format('woff2'); unicode-range: U+0102-0103, U+0110-0111, U+1EA0-1EF9, U+20AB;}@font-face { font-family: 'Nunito'; font-style: normal; font-weight: 400; src: local('Nunito Regular'), local('Nunito-Regular'), url(https://fonts.gstatic.com/s/nunito/v10/XRXV3I6Li01BKofIO-aBXso.woff2) format('woff2'); unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;}@font-face { font-family: 'Nunito'; font-style: normal; font-weight: 400; src: local('Nunito Regular'), local('Nunito-Regular'), url(https://fonts.gstatic.com/s/nunito/v10/XRXV3I6Li01BKofINeaB.woff2) format('woff2'); unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;}@font-face { font-family: 'Sniglet'; font-style: normal; font-weight: 400; src: local('Sniglet Regular'), local('Sniglet-Regular'), url(https://fonts.gstatic.com/s/sniglet/v10/cIf9MaFLtkE3UjaJ9CChYVkH.woff2) format('woff2'); unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;}@font-face { font-family: 'Sniglet'; font-style: normal; font-weight: 400; src: local('Sniglet Regular'), local('Sniglet-Regular'), url(https://fonts.gstatic.com/s/sniglet/v10/cIf9MaFLtkE3UjaJ9C6hYQ.woff2) format('woff2'); unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;}</style></head><body><div id=\"body\" style=\"background-image: url('https://firebasestorage.googleapis.com/v0/b/study-chums.appspot.com/o/img%2Fbluebg.png?alt=media&token=62be1de9-9c75-4212-867d-a09469229df6'); padding: 20px;\"><div id=\"wrapper\" style=\"max-width: 600px; padding: 20px 50px 0px 50px; margin: auto; box-shadow: 1px 1px 10px #d8d8d8; background: #FFF;\"><h1 style=\"font-family: 'Sniglet', sans-serif; color: #24A9E0; text-align: center; font-size: 60px; line-height: 50%;\">Study Chums</h1><h2 style=\"font-family: 'Sniglet', sans-serif; color: #24A9E0;\">Hey, " + recipientName + "!</h2><p style=\"font-family: 'Nunito', sans-serif;\"><strong>" + currentUserDisplayName + "</strong> applied to be your Study Chum! View the request and <strong>" + currentUserDisplayName + "</strong>'s profile by clicking the button below. Happy studying!</p><img src=\"\"><span class=\"button\"><a href=\"https://study-chums.firebaseapp.com/applications.html\" style=\"font-family: 'Sniglet', sans-serif; color: #24A9E0; background-color: #e8e8e8; padding: 15px; text-decoration: none; display: table; margin: 0 auto;\"><br>View Requests <br></a></span><footer style=\"font-family: 'Sniglet', sans-serif; color: #24A9E0; text-align: center; padding: 30px;\">This email was sent to "+ firebaseEmail +".<br><a href=\"https://study-chums.firebaseapp.com/preferences.html\" style=\"text-decoration: none; color: #7f7f7f;\">Click here to update your contact preferences.</a></footer></div></div></body></html>";
+      mailOptions.html = "<html><head><style>@font-face { font-family: 'Nunito'; font-style: normal; font-weight: 400; src: local('Nunito Regular'), local('Nunito-Regular'), url(https://fonts.gstatic.com/s/nunito/v10/XRXV3I6Li01BKofIOuaBXso.woff2) format('woff2'); unicode-range: U+0102-0103, U+0110-0111, U+1EA0-1EF9, U+20AB;}@font-face { font-family: 'Nunito'; font-style: normal; font-weight: 400; src: local('Nunito Regular'), local('Nunito-Regular'), url(https://fonts.gstatic.com/s/nunito/v10/XRXV3I6Li01BKofIO-aBXso.woff2) format('woff2'); unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;}@font-face { font-family: 'Nunito'; font-style: normal; font-weight: 400; src: local('Nunito Regular'), local('Nunito-Regular'), url(https://fonts.gstatic.com/s/nunito/v10/XRXV3I6Li01BKofINeaB.woff2) format('woff2'); unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;}@font-face { font-family: 'Sniglet'; font-style: normal; font-weight: 400; src: local('Sniglet Regular'), local('Sniglet-Regular'), url(https://fonts.gstatic.com/s/sniglet/v10/cIf9MaFLtkE3UjaJ9CChYVkH.woff2) format('woff2'); unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;}@font-face { font-family: 'Sniglet'; font-style: normal; font-weight: 400; src: local('Sniglet Regular'), local('Sniglet-Regular'), url(https://fonts.gstatic.com/s/sniglet/v10/cIf9MaFLtkE3UjaJ9C6hYQ.woff2) format('woff2'); unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;}</style></head><body><div id=\"body\" style=\"background-image: url('https://firebasestorage.googleapis.com/v0/b/study-chums.appspot.com/o/img%2Fbluebg.png?alt=media&token=62be1de9-9c75-4212-867d-a09469229df6'); padding: 20px;\"><div id=\"wrapper\" style=\"max-width: 600px; padding: 20px 50px 0px 50px; margin: auto; box-shadow: 1px 1px 10px #d8d8d8; background: #FFF;\"><img src=\"images/logo.png\" style=\"max-width: 100%; margin: 0 auto; display: table;\"><h2 style=\"font-family: 'Sniglet'; color: #24A9E0;\">Hey, " + recipientName + "!</h2><p style=\"font-family: 'Nunito', sans-serif;\"><strong>" + currentUserDisplayName + "</strong> applied to be your Study Chum! View the request and <strong>" + currentUserDisplayName +"</strong>'s profile by clicking the button below. Happy studying!</p><img src=\"\"><a href=\"https://study-chums.firebaseapp.com/applications.html\" style=\"display: table; margin: 0 auto;\"><img class=\"button\" src=\"images/button.png\" style=\"max-width:200px;\"></a><footer style=\"font-family: 'Sniglet'; color: #24A9E0; text-align: center; padding: 30px;\">This email was sent to " + firebaseEmail + ".<br><a href=\"https://study-chums.firebaseapp.com/preferences.html\" style=\"text-decoration: none; color: #7f7f7f;\">Click here to update your contact preferences.</a></footer></div></div></body></html>";
 
       mailTransport.sendMail(mailOptions, (err, response) =>{
         if (err) {
@@ -83,30 +83,7 @@ exports.sendRequestEmail = functions.database.ref('Applications/{uid}/Sent/{valu
       throw new Error("Error with promise all!!");
     });
     
-  });
-    
-  var recipientEmailRef = userDataRef.child("Users/"+recipientID+"/email");
-  // Input email promise
-  var getInputEmail = recipientEmailRef.once("value", (snapshot) => {
-    inputEmail = snapshot.val(); 
-  }).then(value => {
-    return value;
-  }).catch(error => {
-    console.log(error);
   });  
-  
-  getInputEmail.then(value => {
-    if (value) {
-      
-      
-      return value;
-    }
-    else {
-      throw new Error("Couldnt get input email!");
-    }
-  }).catch(error => {
-    console.log(error.message);
-  });    
   
   return null;
 });
