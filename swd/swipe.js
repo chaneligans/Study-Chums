4149cbfbd754ef38b176f9628b229d116aca67ce
Austin Tao <32249785+MrAlphalfa97@users.noreply.github.com>
2019-04-24 15:02:37 -0700

    a new test
    - test 'swipes' left and right on homepage
    - tests the 'show bio' button (gave the button an id to help for this)

--- swd/swipe.js
+++ swd/swipe.js
@@ -1,15 +1,9 @@
-/*requires:
-- a webdriver <chromedriver> (download and set file folder location as path variable)
-- selenium webdriver (npm install selenium-webdriver)
-- mocha              (npm install mocha)
-*/
-
 var assert = require('assert');
 var {suite} = require('selenium-webdriver/testing');
 var {Builder, Capabilities, until, By} = require('selenium-webdriver');
 
-var url = 'https://study-chums.firebaseapp.com/'
 var expected = 'Study Chums';
+var url = 'https://study-chums.firebaseapp.com/'
 
 const delay = t => new Promise(resolve => setTimeout(resolve,t));
 
@@ -20,38 +14,29 @@ var json = JSON.parse(fs.readFileSync(file, "utf8"));
 var user = json.user;
 var pass = json.pass;
 
-// run the suite
 suite(function(env) {
+  describe('Swiping through users', function() {
 
-  describe('Login to Study Chums', function() {
     var browser;
-
     before(function(done) {
-
       browser = new Builder().
         withCapabilities(Capabilities.chrome()).build();
-
       browser.get(url);
-      delay(500).then(() => {
-        console.log('Opening in browser: ' + url);
-      });
       done();
     });
-
     it('Should expect the login page', function(done) {
       browser.getTitle().then((title) => {
         assert.equal(title, expected);
-        console.log('browser is at login page');
+        console.log('Browser is at login page');
       }, (err)=> {
         console.log('the title did not match the expected: ' + err.message);
       });
       done();
     });
-
     it('Should expect the title for homepage after logging in', function(done) {
       let loginButton = browser.findElement(By.id('loginbtn'));
       loginButton.click();
-      delay(1000).then(() => {console.log('Logging in...')});
+      // delay(1000).then(() => {console.log('Logging in...')});
 
       let main = browser.getWindowHandle();
 
@@ -72,7 +57,7 @@ suite(function(env) {
         delay(5500).then(() => {
           browser.wait(until.elementLocated(By.id('homelink')), 2500).
           then(() => {
-            console.log('Logging in through FB...');
+            // console.log('Logging in through FB...');
             browser.findElement(By.id('email')).sendKeys(user);
             browser.findElement(By.id('pass')).sendKeys(pass);
             browser.findElement(By.id('loginbutton')).click();
@@ -80,7 +65,7 @@ suite(function(env) {
         });
 
         delay(13000).then(() => {
-          console.log('Waiting...');
+          // console.log('Waiting...');
           browser.switchTo().window(m);
         });
 
@@ -89,41 +74,75 @@ suite(function(env) {
             browser.getTitle().then((title) => {
               // console.log('title: ' + title);
               assert.equal(title, 'Home | ' + expected);
-              console.log('Made it to homepage');
+              // console.log('Made it to homepage');
             }, (err) => {console.log('title fail (assert) - homepage: ' + err.message)});
           }, (err) => {console.log('title fail (titleIs) - homepage: ' + err.message)});
         });
 
       });
       done();
-    })
+    });
+
+    it('Should swipe left twice', function(done) { //visual confirmation
+      delay(24000).then(() => {
+        browser.findElement(By.id('leftbutton')).click();
+      }); //lag may appear here, so the image might not show immediately (image size)
+
+      delay(30000).then(() => {
+        browser.findElement(By.id('leftbutton')).click();
+      }); // more stable in its punctuality
+
+      done();
+    });
+
+    it('Should swipe right twice', function(done) { //visual confirmation
+      delay(36000).then(() => {
+        browser.findElement(By.id('rightbutton')).click();
+      }); //lag may appear here, so the image might not show immediately (image size)
+
+      delay(42000).then(() => {
+        browser.findElement(By.id('rightbutton')).click();
+      }); // more stable in its punctuality
+
+      done();
+    });
+
+    it ('Should click on the "show bio" button', function(done) { //visual confirmation
+      delay(44500).then(() => {
+        browser.findElement(By.id('biobutton')).click();
+      }); // should be instant
+
+      delay(48000).then(() => {
+        browser.findElement(By.id('biobutton')).click();
+      }); // should also be instant, but it has a 3.5s delay to it
+
+      done();
+    });
 
     it('Should see the login page after logging out', function(done) {
-      delay(42500).then(() => {
-        console.log('Logging out...');
+      delay(55000).then(() => {
+        // console.log('Logging out...');
         browser.findElement(By.linkText('Sign Out')).click();
       });
 
-      delay(43000).then(() => {
+      delay(56500).then(() => {
         browser.wait(until.titleIs(expected)).then(() => {
           browser.getTitle().then((title) => {
             // console.log('title: ' + title);
             assert.equal(title, expected);
-            console.log('Made it to login page');
+            // console.log('Made it to login page');
           }, (err) => {console.log('title fail (assert) - login page: ' + err.message)});
         }, (err) => {console.log('title fail (titleIs) - login page: ' + err.message)});
       });
 
       done();
     });
-
     after(function(done) {
-      delay(50000).then(() => {
+      delay(60000).then(() => {
         browser.quit();
         console.log('Browser closed');
       });
       done();
     });
   });
-
-});
+})
