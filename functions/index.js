427be823bbb4eb3cff17f8e760780a13b1b04931
chaneligans <37652117+chaneligans@users.noreply.github.com>
2019-04-23 18:21:34 -0700

    added contacts preference page

--- functions/index.js
+++ functions/index.js
@@ -37,6 +37,7 @@ exports.sendRequestEmail = functions.database.ref('Applications/{uid}/Sent/{valu
   
   var recipientName;
   var recipientID;
+  var subscriptionPreference;
   var recipient = change.after.forEach((snapshot) => {
     recipientID = snapshot.ref.parent.key;
 
@@ -50,34 +51,60 @@ exports.sendRequestEmail = functions.database.ref('Applications/{uid}/Sent/{valu
       console.log(error);
     }); 
     
-    // Firebase email promise
-    var getFirebaseEmail = admin.auth().getUser(snapshot.ref.parent.key).then(user => {
-      firebaseEmail = user.email;
-      console.log("Firebase email: ", firebaseEmail);
-      return firebaseEmail;
+    var recipientEmailRef = userDataRef.child("Users/"+snapshot.ref.parent.key+"/email");
+    var getRecipientEmail = recipientEmailRef.once("value", (snapshot) => {
+      inputEmail = snapshot.val(); 
+    }).then(value => {
+      return value;
     }).catch(error => {
-      throw new Error("Error fetching user data: ", error);
+      console.log(error);
     });
     
-    Promise.all([getRecipientName, getFirebaseEmail]).then(values => {
-      // Send mail
-      var mailOptions = {
-        from: `${APP_NAME} <noreply@firebase.com>`,
-        to: email,
-      };
+    var recipientSubscriptionRef = userDataRef.child("Users/"+snapshot.ref.parent.key+"/subscription");
+    var getRecipientSubscription = recipientSubscriptionRef.once("value", (snapshot) => {
+      subscriptionPreference = snapshot.val(); 
+    }).then(value => {
+      return value;
+    }).catch(error => {
+      console.log(error);
+    }); 
+    
+    
+    // Firebase email promise
+//    var getFirebaseEmail = admin.auth().getUser(snapshot.ref.parent.key).then(user => {
+//      firebaseEmail = user.email;
+//      console.log("Firebase email: ", firebaseEmail);
+//      return firebaseEmail;
+//    }).catch(error => {
+//      throw new Error("Error fetching user data: ", error);
+//    });
+    
+    Promise.all([getRecipientName, getRecipientEmail, getRecipientSubscription]).then(values => {
+      email = inputEmail;
+      
+      if (subscriptionPreference === true) {
+        // Send mail
+        var mailOptions = {
+          from: `${APP_NAME} <noreply@firebase.com>`,
+          to: email,
+        };
 
-      mailOptions.subject = `You have a new match request! | ${APP_NAME}`;
+        mailOptions.subject = `You have a new match request! | ${APP_NAME}`;
 
-      mailOptions.html = "<html><head><style>@font-face { font-family: 'Nunito'; font-style: normal; font-weight: 400; src: local('Nunito Regular'), local('Nunito-Regular'), url(https://fonts.gstatic.com/s/nunito/v10/XRXV3I6Li01BKofIOuaBXso.woff2) format('woff2'); unicode-range: U+0102-0103, U+0110-0111, U+1EA0-1EF9, U+20AB;}@font-face { font-family: 'Nunito'; font-style: normal; font-weight: 400; src: local('Nunito Regular'), local('Nunito-Regular'), url(https://fonts.gstatic.com/s/nunito/v10/XRXV3I6Li01BKofIO-aBXso.woff2) format('woff2'); unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;}@font-face { font-family: 'Nunito'; font-style: normal; font-weight: 400; src: local('Nunito Regular'), local('Nunito-Regular'), url(https://fonts.gstatic.com/s/nunito/v10/XRXV3I6Li01BKofINeaB.woff2) format('woff2'); unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;}@font-face { font-family: 'Sniglet'; font-style: normal; font-weight: 400; src: local('Sniglet Regular'), local('Sniglet-Regular'), url(https://fonts.gstatic.com/s/sniglet/v10/cIf9MaFLtkE3UjaJ9CChYVkH.woff2) format('woff2'); unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;}@font-face { font-family: 'Sniglet'; font-style: normal; font-weight: 400; src: local('Sniglet Regular'), local('Sniglet-Regular'), url(https://fonts.gstatic.com/s/sniglet/v10/cIf9MaFLtkE3UjaJ9C6hYQ.woff2) format('woff2'); unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;}</style></head><body><div id=\"body\" style=\"background-image: url('https://firebasestorage.googleapis.com/v0/b/study-chums.appspot.com/o/img%2Fbluebg.png?alt=media&token=62be1de9-9c75-4212-867d-a09469229df6'); padding: 20px;\"><div id=\"wrapper\" style=\"max-width: 600px; padding: 20px 50px 0px 50px; margin: auto; box-shadow: 1px 1px 10px #d8d8d8; background: #FFF;\"><img src=\"images/logo.png\" style=\"max-width: 100%; margin: 0 auto; display: table;\"><h2 style=\"font-family: 'Sniglet'; color: #24A9E0;\">Hey, " + recipientName + "!</h2><p style=\"font-family: 'Nunito', sans-serif;\"><strong>" + currentUserDisplayName + "</strong> applied to be your Study Chum! View the request and <strong>" + currentUserDisplayName +"</strong>'s profile by clicking the button below. Happy studying!</p><img src=\"\"><a href=\"https://study-chums.firebaseapp.com/applications.html\" style=\"display: table; margin: 0 auto;\"><img class=\"button\" src=\"images/button.png\" style=\"max-width:200px;\"></a><footer style=\"font-family: 'Sniglet'; color: #24A9E0; text-align: center; padding: 30px;\">This email was sent to " + firebaseEmail + ".<br><a href=\"https://study-chums.firebaseapp.com/preferences.html\" style=\"text-decoration: none; color: #7f7f7f;\">Click here to update your contact preferences.</a></footer></div></div></body></html>";
+        mailOptions.html = "<html><head><style>@font-face { font-family: 'Nunito'; font-style: normal; font-weight: 400; src: local('Nunito Regular'), local('Nunito-Regular'), url(https://fonts.gstatic.com/s/nunito/v10/XRXV3I6Li01BKofIOuaBXso.woff2) format('woff2'); unicode-range: U+0102-0103, U+0110-0111, U+1EA0-1EF9, U+20AB;}@font-face { font-family: 'Nunito'; font-style: normal; font-weight: 400; src: local('Nunito Regular'), local('Nunito-Regular'), url(https://fonts.gstatic.com/s/nunito/v10/XRXV3I6Li01BKofIO-aBXso.woff2) format('woff2'); unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;}@font-face { font-family: 'Nunito'; font-style: normal; font-weight: 400; src: local('Nunito Regular'), local('Nunito-Regular'), url(https://fonts.gstatic.com/s/nunito/v10/XRXV3I6Li01BKofINeaB.woff2) format('woff2'); unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;}@font-face { font-family: 'Sniglet'; font-style: normal; font-weight: 400; src: local('Sniglet Regular'), local('Sniglet-Regular'), url(https://fonts.gstatic.com/s/sniglet/v10/cIf9MaFLtkE3UjaJ9CChYVkH.woff2) format('woff2'); unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;}@font-face { font-family: 'Sniglet'; font-style: normal; font-weight: 400; src: local('Sniglet Regular'), local('Sniglet-Regular'), url(https://fonts.gstatic.com/s/sniglet/v10/cIf9MaFLtkE3UjaJ9C6hYQ.woff2) format('woff2'); unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;}</style></head><body><div id=\"body\" style=\"background-image: url('https://firebasestorage.googleapis.com/v0/b/study-chums.appspot.com/o/img%2Fbluebg.png?alt=media&token=62be1de9-9c75-4212-867d-a09469229df6'); padding: 20px;\"><div id=\"wrapper\" style=\"max-width: 600px; padding: 20px 50px 0px 50px; margin: auto; box-shadow: 1px 1px 10px #d8d8d8; background: #FFF;\"><img src=\"https://firebasestorage.googleapis.com/v0/b/study-chums.appspot.com/o/img%2Flogo.png?alt=media&token=f8d955ee-e722-4b32-a505-d8f57fc0e20b\" style=\"max-width: 100%; margin: 0 auto; display: table;\"><h2 style=\"font-family: 'Sniglet'; color: #24A9E0;\">Hey, " + recipientName + "!</h2><p style=\"font-family: 'Nunito', sans-serif;\"><strong>" + currentUserDisplayName + "</strong> applied to be your Study Chum! View the request and <strong>" + currentUserDisplayName +"</strong>'s profile by clicking the button below. Happy studying!</p><img src=\"\"><a href=\"https://study-chums.firebaseapp.com/applications.html\" style=\"display: table; margin: 0 auto;\"><img class=\"button\" src=\"https://firebasestorage.googleapis.com/v0/b/study-chums.appspot.com/o/img%2Fbutton.png?alt=media&token=098a5a3f-f363-4b3a-8f67-b11ec4bd1dfe\" style=\"max-width:200px;\"></a><footer style=\"font-family: 'Sniglet'; color: #24A9E0; text-align: center; padding: 30px;\">This email was sent to " + email + ".<br><a href=\"https://study-chums.firebaseapp.com/preferences.html\" style=\"text-decoration: none; color: #7f7f7f;\">Click here to update your contact preferences.</a></footer></div></div></body></html>";
 
-      mailTransport.sendMail(mailOptions, (err, response) =>{
-        if (err) {
-          console.log('Could not send request email to', email);
-        }
-        else{
-          console.log('Request email sent to:', email);
-        }
-      });
+        mailTransport.sendMail(mailOptions, (err, response) =>{
+          if (err) {
+            console.log('Could not send request email to', email);
+          }
+          else{
+            console.log('Request email sent to:', email);
+          }
+        });  
+      } else {
+        console.log('Email preference for ' + email + 'is set to false');
+      }
+      
       return values;
     }).catch(error => {
       throw new Error("Error with promise all!!");
---
+++ public/scripts/edit_email_preferences.js
@@ -0,0 +1,87 @@
+function updateEmailPreferences() {
+  $('html').addClass('waiting');
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      let email_in = document.getElementById("email").value;
+      let subscribe = document.getElementById("subscribe").checked;
+      let unsubscribe = document.getElementById("unsubscribe").checked;
+
+      this.userId = user.uid;
+
+      // execute only if something is there
+      if (email_in !== undefined) {
+        updateEmail(this.userId, email_in);}
+      if (subscribe) {
+        console.log(email_in + ' has subscribed to emails');
+        updateSubscription(this.userId, true);
+      }
+      else {
+        console.log(email_in + ' has unsubscribed to emails');
+        updateSubscription(this.userId, false);
+      }
+      console.log("Updates sent to database.");
+      setTimeout(function() {
+        alert("Email preferences updated.");
+        location.href = "home.html";
+      }, 1000);
+    } else {
+      console.log("Who are you? And why do you have access to this?");
+    }                              
+  });
+}
+
+function updateEmail(user, email_in) {
+  firebase.auth().onAuthStateChanged(function(user) {
+    console.log("Updating email for user id ", user.uid);
+    // update email for this 'user' with email_in
+
+    let ref = firebase.database().ref("Users/" + this.userId);
+    this.userId = user.uid;
+
+    //realtime database
+    ref.update({
+      email: email_in,
+      "email": email_in
+    }, function(error) {
+      if (error) {
+        console.log("Update failed - email to " + email_in);
+      } else {
+        console.log("Update suceeded - email to " + email_in);
+      }
+    });
+  });
+}  
+
+
+function updateSubscription(user, preference) {
+  firebase.auth().onAuthStateChanged(function(user) {
+    console.log("Updating subscription preferences for user id ", user.uid);
+
+    let ref = firebase.database().ref("Users/" + this.userId);
+    this.userId = user.uid;
+    //realtime database
+    if (preference === false) {
+      ref.update({
+        subscribed: false,
+        "subscribed": false
+      }, function(error){
+        if (error) {
+          console.log("Update failed - email pref to false");
+        } else {
+          console.log("Update succeeded - email pref to false");
+        }
+      }); 
+    } else {
+      ref.update({
+        subscribed: true,
+        "subscribed": true
+      }, function(error){
+        if (error) {
+          console.log("Update failed - email pref to true");
+        } else {
+          console.log("Update succeeded - email pref to true");
+        }
+      });
+    }
+  });
+}
