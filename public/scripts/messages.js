69810f6c10959f6813b759f916abfc8ce7f772f7
Samantha Wiley <samantha.wiley@student.csulb.edu>
2019-05-09 16:08:03 -0700

    Open Chat Room Function

--- public/scripts/messages.js
+++ public/scripts/messages.js
@@ -3,9 +3,12 @@ var roomID = "123Test";
 
 
 //if user opens existing chatroom 
-function openChatRoom() {
-  //set roomID
+function openChatRoom(roomID) {
+  db.collection("Users").doc(user.uid).update({
+    currentChatRoom: roomID,
+  });
 
+  loadChatHistory();
 }
 
 //if user creates new chatroom
@@ -13,28 +16,29 @@ function createChatRoom(topic) {
     firebase.auth().onAuthStateChanged(user => {
         if(user){
             const db = firebase.firestore();
-            let room;
-            //add user self first, and later call the updateUser to add more User
     
             db.collection("ChatRooms").add({
                 topic: topic
             })
             .then(function(docRef) {
                 console.log("ChatRoom created with key --- ", docRef.id);
-                room = docRef.id;
-                roomID = room;
+                const roomID = docRef.id;
 
                 db.collection("Users").doc(user.uid).get(result => {
                   let name = result.data().name;
 
-                  db.collection("ChatRooms").doc(room).collection("Users").doc(user.uid)
+                  db.collection("ChatRooms").doc(roomID).collection("Users").doc(user.uid)
                   .set({
                     name: name,
                   });
                 });
 
-                db.collection("Users").doc(user.uid).collection("ChatRooms").doc(room).set({
+                db.collection("Users").doc(user.uid).collection("ChatRooms").doc(roomID).set({
                   topic: topic,
+                });
+
+                db.collection("Users").doc(user.uid).update({
+                  currentChatRoom: roomID,
                 });
             })
             .catch(function(error) {
@@ -61,27 +65,30 @@ function updateUsers(userId){
 }
 
 function loadChatHistory() {
-  let messages = [];
-  const db = firebase.firestore();
+  firebase.auth().onAuthStateChanged(user => {
+    const db = firebase.firestore();
 
-  db.collection("ChatRooms").doc(roomID).collection("Messages")
-  .get().then(function(querySnapshot) {
-      querySnapshot.forEach(function(doc) {
-          console.log(doc.id, " => ", doc.data());
-          messages.push({
-            senderID: doc.data().senderID,
-            senderName: doc.data().senderName,
-            time: doc.data().time.toDate(),
-            message: doc.data().message,
-          });
-      });
+    let messages = [];
 
-      Promise.all(messages).then(results => {
-        displayMessages(results);
-      });
-  })
-  .catch(function(error) {
-      console.log("Error getting documents: ", error);
+    db.collection("ChatRooms").doc(roomID).collection("Messages")
+    .get().then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            console.log(doc.id, " => ", doc.data());
+            messages.push({
+              senderID: doc.data().senderID,
+              senderName: doc.data().senderName,
+              time: doc.data().time.toDate(),
+              message: doc.data().message,
+            });
+        });
+
+        Promise.all(messages).then(results => {
+          displayMessages(results);
+        });
+    })
+    .catch(function(error) {
+        console.log("Error getting documents: ", error);
+    });
   });
 }
 
