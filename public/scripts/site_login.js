01a8bdf6813b91288fd07267aafd88ed1290113a
Samantha Wiley <samantha.wiley@student.csulb.edu>
2019-10-21 20:33:02 -0700

    Refactor Site Login

--- public/scripts/site_login.js
+++ public/scripts/site_login.js
@@ -1,66 +1,95 @@
 function facebookSignIn() {
   console.log("clicked 'facebook sign in'");
 
-  var provider = new firebase.auth.FacebookAuthProvider();
+  const provider = new firebase.auth.FacebookAuthProvider();
   firebase.auth().signInWithPopup(provider).then(function(result) {
     console.log("Result: ",result);
 
     // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-    var token = result.credential.accessToken;
+    const token = result.credential.accessToken;
 
     // The signed-in user info.
-    var user = result.user;
+    const user = result.user;
     console.log("Successfully signed in " + user.displayName);
 
     // Redirect after signing in
     if(result.additionalUserInfo.isNewUser) {
-        var totalUsers;
-        var totalRef = firebase.database().ref("TotalUsers");
-        totalRef.once("value", function(data) {
-          totalUsers = data.val().total;
-          var index_in = totalUsers;
-          totalUsers = totalUsers + 1;
+      const index = incrementTotalUsers();
+      setIndex(user, index);
 
-          var userRef = firebase.database().ref("Users/" + user.uid);
-          userRef.update({
-            index: index_in,
-            "index": index_in,
-            "currentIndex": 0,
-            currentIndex: 0
-          }, function(error) {
-            if (error) {
-              console.log("Update failed - index to " + index_in);
-            } else {
-              console.log("Update suceeded - index to " + index_in);
-            }
-          });
-
-          totalRef.update({
-            total: totalUsers,
-            "total": totalUsers
-          }, function(error) {
-            if (error) {
-              console.log("Update failed - total to " + totalUsers);
-              alert("Something went wrong! Please try again.\nError: " + error);
-            } else {
-              console.log("Update suceeded - total to " + totalUsers);
-              setTimeout(function() {
-                location.href = "create_profile.html";
-              }, 1000);
-            }
-          });
-        });
-    } else {
+      setTimeout(() => {
+        location.href = "create_profile.html";
+      }, 1000);
+    } 
+    else {
       window.location.href = "home.html";
     }
 
   }).catch(function(error) {
     // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
+    const errorCode = error.code;
+    const errorMessage = error.message;
     // The email of the user's account used.
-    var email = error.email;
+    const email = error.email;
     // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
+    const credential = error.credential;
   });
 }
+
+function incrementTotalUsers() {
+  let totalUsers;
+  const totalRef = firebase.database().ref("TotalUsers");
+  totalRef.once("value", function(data) {
+    totalUsers = data.val().total;
+    totalUsers = totalUsers + 1;
+
+    totalRef.update({
+      total: totalUsers,
+      "total": totalUsers
+    }, error => {
+      if (error) {
+        console.log("Update failed - total to " + totalUsers);
+        alert("Something went wrong! Please try again.\nError: " + error);
+      } else {
+        console.log("Update suceeded - total to " + totalUsers);
+      }
+    });
+  });
+
+  Promise.resolve(totalUsers).then(result => {
+    return result-1;
+  });
+}
+
+function setIndex(user, index) {
+  const userRef = firebase.database().ref("Users/" + user.uid);
+  userRef.update({
+    index: index,
+    "index": index,
+    "currentIndex": 0,
+    currentIndex: 0
+  }, error => {
+    if (error) {
+      console.log("Update failed - index to " + index);
+    } else {
+      console.log("Update suceeded - index to " + index);
+    }
+  });
+}
+
+// function createUserWithEmailAndPassword() {
+//   const email_in = document.getElementById("email").value;
+//   const password_in = document.getElementById("password").value;
+
+//   firebase.auth().createUserWithEmailAndPassword(email_in, password_in)
+//   .catch(function(error) {
+//     // Handle Errors here.
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//     // ...
+//   });
+// }
+
+// function emailAndPasswordSignIn() {
+
+// }
